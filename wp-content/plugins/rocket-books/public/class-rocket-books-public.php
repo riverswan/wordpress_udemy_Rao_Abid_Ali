a995e5823a9fe05668abbb699c7e274c2dfbcf52
Pavel Lebedev <maxwinrate@gmail.com>
2020-03-06 18:23:41 +0200

    Working on plugin

--- wp-content/plugins/rocket-books/public/class-rocket-books-public.php
+++ wp-content/plugins/rocket-books/public/class-rocket-books-public.php
@@ -27,7 +27,7 @@ class Rocket_Books_Public {
 	 *
 	 * @since    1.0.0
 	 * @access   private
-	 * @var      string    $plugin_name    The ID of this plugin.
+	 * @var      string $plugin_name The ID of this plugin.
 	 */
 	private $plugin_name;
 
@@ -36,16 +36,17 @@ class Rocket_Books_Public {
 	 *
 	 * @since    1.0.0
 	 * @access   private
-	 * @var      string    $version    The current version of this plugin.
+	 * @var      string $version The current version of this plugin.
 	 */
 	private $version;
 
 	/**
 	 * Initialize the class and set its properties.
 	 *
+	 * @param string $plugin_name The name of the plugin.
+	 * @param string $version The version of this plugin.
+	 *
 	 * @since    1.0.0
-	 * @param      string    $plugin_name       The name of the plugin.
-	 * @param      string    $version    The version of this plugin.
 	 */
 	public function __construct( $plugin_name, $version ) {
 
@@ -74,6 +75,10 @@ class Rocket_Books_Public {
 		 */
 
 		wp_enqueue_style( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'css/rocket-books-public.css', array(), $this->version, 'all' );
+		if ( is_singular( 'book' ) ) {
+			wp_enqueue_style( $this->plugin_name . '-single-book', plugin_dir_url( __FILE__ ) . 'css/rocket-book-book-single.css', array(), $this->version, 'all' );
+		};
+
 
 	}
 
--- wp-content/plugins/rocket-books/templates/single/content-book.php
+++ wp-content/plugins/rocket-books/templates/single/content-book.php
@@ -1,3 +1,40 @@
-<?php declare( strict_types=1 );
-echo "i am from new template \n";
+<?php
+/**
+ * The template part for displaying single posts
+ *
+ * @package WordPress
+ * @subpackage Twenty_Sixteen
+ * @since Twenty Sixteen 1.0
+ */
+?>
 
+<article id="post-<?php the_ID(); ?>" <?php post_class( 'single-book-container' ); ?>>
+
+    <div class="book-meta-container">
+        <div class="book-entry-img">
+			<?php the_post_thumbnail(); ?>
+        </div>
+    </div>
+
+    <div class="book-entry-content">
+		<?php
+		the_content();
+
+		?>
+    </div><!-- .entry-content -->
+
+    <footer class="book-entry-footer">
+
+		<?php
+		edit_post_link(
+			sprintf(
+			/* translators: %s: Name of current post */
+				__( 'Edit<span class="screen-reader-text"> "%s"</span>', 'rocket-books' ),
+				get_the_title()
+			),
+			'<span class="edit-link">',
+			'</span>'
+		);
+		?>
+    </footer><!-- .entry-footer -->
+</article><!-- #post-<?php the_ID(); ?> -->
