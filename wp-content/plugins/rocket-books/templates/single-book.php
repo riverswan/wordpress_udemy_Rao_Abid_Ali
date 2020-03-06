71573b47dd56682558c49bb7b88c1a7870ab6182
Pavel Lebedev <maxwinrate@gmail.com>
2020-03-06 18:08:01 +0200

    Working on plugin

--- wp-content/plugins/rocket-books/templates/single-book.php
+++ wp-content/plugins/rocket-books/templates/single-book.php
@@ -1,8 +1,39 @@
-<?php declare( strict_types=1 );
-get_header();
-echo "I am a single book template \n";
-echo "<pre>";
-var_dump( __FILE__ );
-echo "</pre>";
-get_footer();
+<?php
+/**
+ * The template for displaying all single posts and attachments
+ *
+ * @package WordPress
+ * @subpackage Twenty_Sixteen
+ * @since Twenty Sixteen 1.0
+ */
 
+get_header(); ?>
+
+
+<?php
+// Start the loop.
+while ( have_posts() ) :
+	the_post();
+	include ROCKET_BOOKS_BASE_DIR . 'templates/single/content-book.php';
+
+
+	// Previous/next post navigation.
+	the_post_navigation(
+		array(
+			'next_text' => '<span class="meta-nav" aria-hidden="true">' . __( 'Next', 'rocket-books' ) . '</span> ' .
+						   '<span class="screen-reader-text">' . __( 'Next post:', 'rocket-books' ) . '</span> ' .
+						   '<span class="post-title">%title</span>',
+			'prev_text' => '<span class="meta-nav" aria-hidden="true">' . __( 'Previous', 'rocket-books' ) . '</span> ' .
+						   '<span class="screen-reader-text">' . __( 'Previous post:', 'rocket-books' ) . '</span> ' .
+						   '<span class="post-title">%title</span>',
+		)
+	);
+
+
+	// End of the loop.
+endwhile;
+?>
+
+
+<?php
+get_footer();
