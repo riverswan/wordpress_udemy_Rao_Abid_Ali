190f4506a69638aaef8a97078261eebc4053779a
Pavel Lebedev <maxwinrate@gmail.com>
2020-03-09 21:47:07 +0200

    Working on plugin

--- wp-content/plugins/rocket-books/includes/class-rocket-books-shortcodes.php
+++ wp-content/plugins/rocket-books/includes/class-rocket-books-shortcodes.php
@@ -24,8 +24,9 @@ if ( ! class_exists( 'Rocket_Books_Shortcodes' ) ) {
 
 			$args = shortcode_atts(
 				array(
-					'limit'  => get_option( 'posts_per_page' ),
-					'column' => 3,
+					'limit'   => get_option( 'posts_per_page' ),
+					'column'  => 3,
+					'bgcolor' => '#f6f6f6',
 				),
 				$args,
 				'book_list'
@@ -34,15 +35,18 @@ if ( ! class_exists( 'Rocket_Books_Shortcodes' ) ) {
 			$loop_args = array(
 				'post_type'      => 'book',
 				'posts_per_page' => $args['limit'],
-				'bgcolor'        => '#f6f6f6',
 			);
 			$loop      = new WP_Query( $loop_args );
 
 			$grid_column = rbr_get_column_class( $args['column'] );
 
+			$css = ".cpt-cards.cpt-shortcodes .cpt-card {background-color: {$args['bgcolor']};}";
+			wp_add_inline_style( $this->plugin_name . '-shortcodes', $css );
+			wp_enqueue_style( $this->plugin_name . '-shortcodes' );
+
 			ob_start();
 			?>
-			<div class="cpt-cards <?php echo sanitize_html_class( $grid_column ); ?>">
+			<div class="cpt-cards cpt-shortcodes <?php echo sanitize_html_class( $grid_column ); ?>">
 				<?php
 				// Start the Loop.
 				while ( $loop->have_posts() ) :
@@ -61,7 +65,10 @@ if ( ! class_exists( 'Rocket_Books_Shortcodes' ) ) {
 		}
 
 		public function register_style() {
-		    wp_register_style($this->plugin_name . '-shortcodes','','','','');
+			wp_register_style(
+				$this->plugin_name . '-shortcodes',
+				ROCKET_BOOKS_URL_PATH . 'public/css/rocket-books-shortcodes.css',
+			);
 		}
 	}
 }
