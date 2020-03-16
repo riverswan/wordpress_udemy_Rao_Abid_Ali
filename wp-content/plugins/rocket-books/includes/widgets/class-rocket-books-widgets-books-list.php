69913d6eb58fee3d833c358cce23c8597ae863b8
Pavel Lebedev <maxwinrate@gmail.com>
2020-03-16 19:53:49 +0200

    Working on plugin

--- wp-content/plugins/rocket-books/includes/widgets/class-rocket-books-widgets-books-list.php
+++ wp-content/plugins/rocket-books/includes/widgets/class-rocket-books-widgets-books-list.php
@@ -1,47 +1,55 @@
 <?php
 
-class Rocket_Books_Widgets_Books_List extends WP_Widget {
 
-	/**
-	 * Sets up the widgets name etc
-	 */
-	public function __construct() {
-		$widget_ops = array(
-			'classname'   => 'my_widget',
-			'description' => 'My Widget is awesome',
-		);
-		parent::__construct( 'my_widget', 'My Widget', $widget_ops );
+if ( ! class_exists( 'Rocket_Books_Widgets_Books_List ' ) ) {
+
+	class Rocket_Books_Widgets_Books_List extends WP_Widget {
+
+		/**
+		 * Sets up the widgets name etc
+		 */
+		public function __construct() {
+			$widget_ops = array(
+				'classname'   => 'rbr_books_list_class',
+				'description' => __( 'Display Rocket Books list', 'rocket-books' ),
+			);
+			parent::__construct( 'rbr_books_list', __( 'Books List', 'rocket-books' ), $widget_ops );
+		}
+
+		/**
+		 * Outputs the content of the widget
+		 *
+		 * @param array $args
+		 * @param array $instance
+		 */
+		public function widget( $args, $instance ) {
+			echo "<pre>";
+			print_r($args);
+			echo "</pre>";
+			die();
+			echo "SOme text from widget \n";
+		}
+
+		/**
+		 * Outputs the options form on admin
+		 *
+		 * @param array $instance The widget options
+		 */
+		public function form( $instance ) {
+			echo "SOme text from form \n";
+		}
+
+		/**
+		 * Processing widget options on save
+		 *
+		 * @param array $new_instance The new options
+		 * @param array $old_instance The previous options
+		 *
+		 * @return array
+		 */
+		public function update( $new_instance, $old_instance ) {
+			// processes widget options to be saved
+		}
 	}
 
-	/**
-	 * Outputs the content of the widget
-	 *
-	 * @param array $args
-	 * @param array $instance
-	 */
-	public function widget( $args, $instance ) {
-		// outputs the content of the widget
-	}
-
-	/**
-	 * Outputs the options form on admin
-	 *
-	 * @param array $instance The widget options
-	 */
-	public function form( $instance ) {
-		// outputs the options form on admin
-	}
-
-	/**
-	 * Processing widget options on save
-	 *
-	 * @param array $new_instance The new options
-	 * @param array $old_instance The previous options
-	 *
-	 * @return array
-	 */
-	public function update( $new_instance, $old_instance ) {
-		// processes widget options to be saved
-	}
 }
-
